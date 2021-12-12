14e21aa6aa145b1744b24a0a6c1c7664e08e1c35
elbeicktalat <talatelbeick@gmail.com>
2021-12-13 00:41:09 +0100

    add tile widget

---
+++ lib/views/widget/tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:game_2048/models/tile.dart';
+import 'package:game_2048/utils/colors.dart';
+
+class TileWidget extends StatelessWidget {
+  const TileWidget({Key? key, required this.tile}) : super(key: key);
+  final Tile tile;
+
+  @override
+  Widget build(BuildContext context) {
+    double tileSize = ((MediaQuery.of(context).size.width - 32.0) - 8.0) / 4;
+
+    int tileValue = tile.value;
+    return Container(
+      width: tileSize - 10.0 * 2,
+      height: tileSize - 10.0 * 2,
+      decoration: BoxDecoration(
+        color: tileColors[tile.value],
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Center(
+        child: Text(
+          (tileValue == 0) ? '' : tileValue.toString(),
+          style: TextStyle(
+            fontSize: 68.0,
+            fontWeight: FontWeight.bold,
+            color: (tileValue <= 4) ? const Color(0xff776e65) : Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}

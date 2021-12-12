1b8724017f8d1d165642490f703914b8bdecf56d
elbeicktalat <talatelbeick@gmail.com>
2021-12-12 23:42:19 +0100

    add colors util

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart' show Color;
+
+Color backgroundColor = const Color(0xfffaf8ef);
+Color boardColor = const Color(0xffbbada0);
+
+Map<int, Color> tileColors = {
+  0: const Color(0xffcdc1b4),
+  2: const Color(0xffeee4da),
+  4: const Color(0xffeee1c9),
+  8: const Color(0xfff3b27a),
+  16: const Color(0xfff69664),
+  32: const Color(0xfff77c5f),
+  64: const Color(0xfff75f3b),
+  128: const Color(0xffedd073),
+  256: const Color(0xffedcc62),
+  512: const Color(0xffedc850),
+  1024: const Color(0xffedc53f),
+  2048: const Color(0xffedc22d),
+};

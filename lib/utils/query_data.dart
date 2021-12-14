48e91a654358a0b118ca1ae079f4eeae85598817
elbeicktalat <talatelbeick@gmail.com>
2021-12-14 12:12:44 +0100

    fix board & toggle sizes.

--- lib/utils/query_data.dart
+++ lib/utils/query_data.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 late MediaQueryData queryData;
 
-double boardSize = queryData.size.width - 16.0 * 2;
+double boardSize = queryData.size.width - 8.0 * 2;
 double tileSize = (boardSize - 8.0) / 4;
-double toggleWidth = queryData.size.width * 0.2;
+double toggleWidth = queryData.size.width * 0.4;
 double toggleHeight = queryData.size.height * 0.08;
